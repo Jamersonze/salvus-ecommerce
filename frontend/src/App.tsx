29d57f48608e4067f90e99ef4e6dd58938ea9966
jamersonze <jamersonze54@gmail.com>
2024-07-17 01:54:24 -0300

    feat: add createform updateform and datachart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,33 @@
 import { Product, columns } from "@/components/app/Columns"
 import { DataTable } from "@/components/app/DataTable"
 import { getProducts } from "./api/Api"
+import { useEffect, useState } from "react"
+import { ProductForm } from "./components/app/ProductForm";
+import { UpdateForm } from "./components/app/UpdateForm";
 
 
 function App() {
-  const data = [] as Product[]
-  getProducts().then((products) => {
-    data.push(...products)
-  })
+  const [data, setData] = useState<Product[]>([]);
+
+  function handleGetProducts() {
+    getProducts().then((products: Product[]) => {
+      if (products) {
+        setData(products);
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
+  useEffect(handleGetProducts, []);
 
   return (
     <section className='container mx-auto py-10'>
-      <DataTable columns={columns} data={data}/>
+      <ProductForm onCreate={handleGetProducts} />
+      <DataTable columns={columns} data={data} />
+      <UpdateForm onUpdate={handleGetProducts} />
     </section>
-  )
+  );
 }
 
 export default App
--- frontend/src/components/app/Columns.tsx
+++ frontend/src/components/app/Columns.tsx
@@ -2,23 +2,9 @@ import { ColumnDef } from "@tanstack/react-table"
 import { ArrowUpDown } from "lucide-react"
  
 import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
 
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
-import { deleteProduct } from "@/api/Api"
+import { deleteProduct } from "@/api/Api";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
 export type Product = {
     id: number;
     name: string;
@@ -27,27 +13,13 @@ export type Product = {
     created_at: Date | null;
 };
 
+export type ProductOnForm = {
+    name?: string;
+    description?: string;
+    price?: number;
+}
+
 export const columns: ColumnDef<Product>[] = [
-    {
-        id: "select",
-        header: ({ table }) => (
-          <Checkbox
-            checked={
-              table.getIsAllPageRowsSelected() ||
-              (table.getIsSomePageRowsSelected() && "indeterminate")
-            }
-            onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-            aria-label="Select all"
-          />
-        ),
-        cell: ({ row }) => (
-          <Checkbox
-            checked={row.getIsSelected()}
-            onCheckedChange={(value) => row.toggleSelected(!!value)}
-            aria-label="Select row"
-          />
-        ),
-    },
     {
         accessorKey: "id",
         header: ({ column }) => {
@@ -66,13 +38,13 @@ export const columns: ColumnDef<Product>[] = [
         accessorKey: "name",
         header: ({ column }) => {
             return (
-            <Button
-                variant="ghost"
-                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-            >
-                Nome
-                <ArrowUpDown className="ml-2 h-4 w-4" />
-            </Button>
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Nome
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
             )
         },
     },
@@ -102,23 +74,13 @@ export const columns: ColumnDef<Product>[] = [
         const product = row.original
     
         return (
-            <AlertDialog>
-                <AlertDialogTrigger asChild>
-                    <Button variant="outline" className="btn-danger">Apagar</Button>
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                    <AlertDialogHeader>
-                        <AlertDialogTitle>Tem certeza?</AlertDialogTitle>
-                        <AlertDialogDescription>
-                            Essa ação não pode ser desfeita.
-                        </AlertDialogDescription>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                        <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                        <AlertDialogAction onClick={() => deleteProduct(product.id)}>Confirmar</AlertDialogAction>
-                    </AlertDialogFooter>
-                </AlertDialogContent>
-            </AlertDialog>
+            <Button
+                variant="destructive"
+                className=""
+                onClick={() => confirm("Tem certeza que deseja apagar este registro?") && deleteProduct(product.id)}
+            >
+                Apagar
+            </Button>
         )
         },
     }
